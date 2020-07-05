3825ac6c4b289a59ef2b3ca7f1d9f3d7aacc71d7
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-05 17:42:25 +0300

    Working with Text
    Chapter 2
    Getting Started with SwiftUI and Working with Text
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIText/ContentView.swift
+++ SwiftUIText/ContentView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+            .fontWeight(.bold)
+            .font(.custom("Marker Felt Thin", size: 40))
+            .foregroundColor(.gray)
+            .multilineTextAlignment(.center)
+            .lineSpacing(10)
+            .padding()
+            .rotationEffect(Angle.degrees(20), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.degrees(60), axis: (x: 1, y: 0, z: 0))
+            .shadow(color: .black, radius: 2, x: 0, y: 15)
     }
 }
 
